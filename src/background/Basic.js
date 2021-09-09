6202b0d847acebf57d8a8d3c8408b8e769dda231
Carlos Garcia <carlosgarcia@192-168-1-164.tpgi.com.au>
2021-09-10 00:52:16 +1000

    3

--- src/background/Basic.js
+++ src/background/Basic.js
@@ -15,10 +15,37 @@ export default function Basic(props) {
     const [ renderer, setRenderer ] = useState(new THREE.WebGLRenderer())
     const [ imgLoader, setImgLoader ] = useState(new THREE.ImageBitmapLoader())
     const [ geometry, setGeometry ] = useState(new THREE.BoxGeometry( 1, 1, 1 ))
-    const [ bulbLight, setBulbLight ] = useState(new THREE.PointLight( 0xffee88, 1, 100, 2 ))
-    const [ bulbMat, setBulbMat ] = useState(new THREE.MeshStandardMaterial( { emissive: 0xffffee, emissiveIntensity: 1, color: 0x000000 } ))
 
-// #6f4775
+
+// scroll + rotate along Y axis (look around horizontally to be implemented)
+    const createCenterSymbol = () => {
+        // POSITION FOR LOGO IF THERE IS ONE - DEFAULT IS OURS?
+        const boxgeometry = new THREE.BoxGeometry( .5, .5, .5 );
+        const material = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
+        const cube = new THREE.Mesh( boxgeometry, material );
+
+        cube.position.set(4, 2.5, 2)
+        cube.lookAt(CAM_POS_X, CAM_POS_Y, CAM_POS_Z) // looks at camera position
+        scene.add( cube );
+
+        return cube
+    }
+    const [ centerSymbol, setCenterSymbol ] = useState(createCenterSymbol()) // to access properties of center symbol
+
+    const createOriginLight = () => {
+        const boxgeometrySmol = new THREE.BoxGeometry( .2, .2, .2 );
+        const materialBaka = new THREE.MeshBasicMaterial({ color: 0xbaca });
+        const cubeSmol = new THREE.Mesh( boxgeometrySmol, materialBaka );
+        cubeSmol.position.set(0,0,0)
+        cubeSmol.lookAt(4, 2.5, 2) // looks at camera position
+
+        scene.add( cubeSmol );
+
+        return cubeSmol
+    }
+    const [ originLight, setOriginLight ] = useState(createOriginLight()) // to access properties of center symbol
+
+
     // init scene/camera/renderer
     const init = () => {
         // makes sure vars are set
@@ -36,37 +63,33 @@ export default function Basic(props) {
         // init camera pos to move up/right of 0,0,0 origin @ rate of various units away
         // (4, 3, 4)
         camera.position.x = CAM_POS_X;
-        camera.position.y = CAM_POS_Y;
-        camera.position.z = CAM_POS_Z;
+        camera.position.y = 4 //CAM_POS_Y;
+        camera.position.z = 6//CAM_POS_Z;
 
         // sets camera to rotate down on x axis (look down slightly)
         camera.rotation.x = -.4;
         camera.rotation.y = 0;
         camera.rotation.z = 0;
 
-        let ambient = new THREE.AmbientLight(0x555555);
-        scene.add(ambient);
+        // let ambient = new THREE.AmbientLight(0xffff, 1);
+        // scene.add(ambient);
 
         renderer.setSize(window.innerWidth,window.innerHeight);
         scene.fog = new THREE.FogExp2('#0a0101', 0.001);
         renderer.setClearColor(scene.fog.color);
         document.body.appendChild(renderer.domElement);
 
-        // POSITION FOR LOGO IF THERE IS ONE - DEFAULT IS OURS?
-        const boxgeometry = new THREE.BoxGeometry( .5, .5, .5 );
-        const material = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
-        const cube = new THREE.Mesh( boxgeometry, material );
-        cube.position.set(4, 2.5, 2)
-        cube.lookAt(CAM_POS_X, CAM_POS_Y, CAM_POS_Z) // looks at camera position
-
-        const boxgeometrySmol = new THREE.BoxGeometry( .2, .2, .2 );
-        const materialBaka = new THREE.MeshBasicMaterial({ color: 0xbaca });
-        const cubeSmol = new THREE.Mesh( boxgeometrySmol, materialBaka );
-        cubeSmol.position.set(0,0,0)
-        cubeSmol.lookAt(4, 2.5, 2) // looks at camera position
 
 
-        scene.add( cubeSmol );
+        // const light = new THREE.PointLight( 0xe444, 100, 100, 1 );
+        // light.position.set( 0,0,0 );
+
+        // scene.add( light );
+        // const sphereSize = 2;
+        // const pointLightHelper = new THREE.PointLightHelper( light, sphereSize );
+        // scene.add( pointLightHelper );
+
+
 
         console.log('init', { scene, camera })
         render();
