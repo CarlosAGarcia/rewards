e5bc1753d4e2c0f4aee078e4086dc95f51bf84e7
Carlos Garcia <carlosgarcia@192-168-1-164.tpgi.com.au>
2021-09-29 01:59:40 +1000

    Changed colo + styling of modal. Added draggable capabilities. Added glow animation

--- src/StyledComponents/modals/ObjectDetailsModal.js
+++ src/StyledComponents/modals/ObjectDetailsModal.js
@@ -2,31 +2,43 @@ import React, { useState, useEffect } from 'react'
 import styled from "styled-components";
 import { useStore } from '../../store'
 import CustomPortal from '../../utilComponents/CustomPortal.js'
+import Draggable from 'react-draggable';
 
-// const ObjectDetailsModalStyled = styled.div`
-//     position: absolute;
-//     top: ${ props => props.position.y };
-//     left: ${ props => props.position.x };
-//     background-color: white;
-//     z-index: 99;
-// `
+
 const ObjectDetailsModalStyled = styled.div`
     position: absolute;
-    top: 0;
-    left: 0;
-    background-color: white;
-    /* width: 20%;
-    height: 15%; */
+    top: 10px; //${props => props.position ? props.position.y : 0};
+    left: 10px; //${props => props.position ? props.position.x : 0};
+    background-color: rgb(191 241 251);
     overflow: scroll;
     opacity: .8;
+    border-radius: 20px;
+    padding: 1rem 2rem;
+
+    animation: glowPink 8s ease-in infinite;
+    &:before {
+        backdrop-filter: blur(10px) saturate(100%) contrast(45%) brightness(130%);
+    }
     .ModalContainer {
         font-family: 'Catamaran', sans-serif;
         width: 21rem;
         height: 17rem;
+        color: #000066;
         .headerContainer {
             width: 100%;
             display: flex;
             justify-content: space-between;
+            .hashText {
+                font-size: 2rem;
+            }
+            .closeBtn {
+                border: none;
+                background: transparent;
+            }
+            .closeBtn, .hashText {
+                display: flex;
+                align-items: center;                
+            }
         }
         .contentContainer {
             padding-top: 2rem;
@@ -34,13 +46,35 @@ const ObjectDetailsModalStyled = styled.div`
                 display: flex;
                 padding: .5rem;
                 border-top: 1px solid #cccc;
+                font-size: 1.5rem;
                 .keyText {
                     margin-right: .5rem;
                 }
             }
         }
     }
+    @-webkit-keyframes glowPink {
+        from {
+            box-shadow: rgb(191 241 251) 0px 4px 15px 3px, rgb(191 241 251) 0px 0px 18px 1px inset;
+        }
+        50% {
+            box-shadow: rgb(191 241 251) 0px 4px 15px 7px, rgb(191 241 251) 0px 0px 18px 0px inset;                   
+        } to {
+            box-shadow: rgb(191 241 251) 0px 4px 15px 3px, rgb(191 241 251) 0px 0px 18px 1px inset;
+        }
+    }
+    @keyframes glowPink {
+        from {
+            box-shadow: rgb(191 241 251) 0px 4px 15px 3px, rgb(191 241 251) 0px 0px 18px 1px inset;
+        } 
+        50% {
+            box-shadow: rgb(191 241 251) 0px 4px 15px 7px, rgb(191 241 251) 0px 0px 18px 0px inset;                  
+        } to {
+            box-shadow: rgb(191 241 251) 0px 4px 15px 3px, rgb(191 241 251) 0px 0px 18px 1px inset;
+        }
+    }
 `
+
 export default function ObjectDetailsModal(props) {
     const { hash, position } = props
 
@@ -69,34 +103,36 @@ export default function ObjectDetailsModal(props) {
 
         const url = canBeLinks.includes(keyUpper) ? `https://etherscan.io/search?f=0&q=${val}` : undefined
         const text = isValue ? `${val} (approx. ${valueInEth}) ETH` : `${val}`
-
+        const textFormatted = text.length > 14 ? `${text.substring(0, 15)}...` : text
         return <a target="_blank" rel="noreferrer" href={url} >
-            {text}
+            {textFormatted}
         </a>
     }
-
+    console.log({ position })
     return (
         <CustomPortal id={'App'} className='customPortalContainer'>
-            <ObjectDetailsModalStyled position={position}>
-                <div className='ModalContainer'>
-                    <div className='headerContainer'>
-                        <div className='hashText'>{hash}</div>
-                        <div className='closeBtn' onClick={() => removeTransactionClicked(hash)}>CLOSE</div>
-                    </div>
-                    <div className='contentContainer'>
-                        {
-                            Object.keys(details).map((key, i) => {
-                                return <div key={`detail-${i}`} className='detailKeyContainer'>
-                                    <div className='keyText'>{`${key.toUpperCase()}: `}</div>
-                                    <div className='keyDetailsContainer' >
-                                        { generateLink(key) }
+            <Draggable>
+                <ObjectDetailsModalStyled position={position}>
+                    <div className='ModalContainer'>
+                        <div className='headerContainer'>
+                            <div className='hashText'>TRANSACTION</div>
+                            <button className='closeBtn' type='button' onClick={() => removeTransactionClicked(hash)}>CLOSE</button>
+                        </div>
+                        <div className='contentContainer'>
+                            { details &&
+                                Object.keys(details).map((key, i) => {
+                                    return <div key={`detail-${i}`} className='detailKeyContainer'>
+                                        <div className='keyText'>{`${key.toUpperCase()}: `}</div>
+                                        <div className='keyDetailsContainer' >
+                                            { generateLink(key) }
+                                        </div>
                                     </div>
-                                </div>
-                            })
-                        }
-                    </div>
-                </div> 
-            </ObjectDetailsModalStyled>
+                                })
+                            }
+                        </div>
+                    </div> 
+                </ObjectDetailsModalStyled>
+            </Draggable>
         </CustomPortal>
     )
 }
--- src/viz/FullWindowMouseTracking.js
+++ src/viz/FullWindowMouseTracking.js
@@ -42,8 +42,6 @@ export default function FullWindowMouseTracking() {
         }
     }, [ transactionsClicked, hashesClicked ])
 
-
-
     return (
         <FullWidthMouseTracking>
             {
